6b6ab18be9d0c215d70fc01f8bc5f539f02aab64
Harsh-palisade <harshdeep.singh930@education.nsw.gov.au>
2026-02-27 21:45:28 +0000

    did all the dashboard also added dark and light theme in the styles.

---
+++ Sports portal/script.js
@@ -0,0 +1,41 @@
+/* Default Light MOde */
+body {
+    font-family: Arial, sans-serif:
+    background: #f4f4f4;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: centre;
+    align-items: centre;
+    height: 100vh;
+
+}
+
+/*Theme Toggle Button */
+.theme-toggle {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    padding: 8px 14px;
+    background: #333;
+    color: white;
+    border: none;
+    border-radius: 6px;
+    cursor: pointer;
+}
+
+/* Login Box */
+form {
+    background: white;
+    padding: 30px;
+    width: 300px;
+    border-radius: 12px;
+    box-shadow: 0 8px 20px rgba(0, 0, 0, 0.15);
+}
+
+h1 {
+    text-align: center;
+    margin-bottom: 20px;
+    color: #333;
+
+}
