301777b54a7636a67570cf88ca96b24bd97b83a3
Harsh-palisade <harshdeep.singh930@education.nsw.gov.au>
2026-04-22 08:43:05 +0000

    Did all the final changes

--- Sports portal/script.js
+++ Sports portal/script.js
@@ -1,41 +1,18 @@
-/* Default Light MOde */
-body {
-    font-family: Arial, sans-serif:
-    background: #f4f4f4;
-    margin: 0;
-    padding: 0;
-    display: flex;
-    justify-content: centre;
-    align-items: centre;
-    height: 100vh;
+function login() {
+    let role = document.getElementById("role").value;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
 
-}
+    if (username === "" || password === "") {
+        alert("Please fill all fields");
+        return;
+    }
 
-/*Theme Toggle Button */
-.theme-toggle {
-    position: absolute;
-    top: 20px;
-    right: 20px;
-    padding: 8px 14px;
-    background: #333;
-    color: white;
-    border: none;
-    border-radius: 6px;
-    cursor: pointer;
-}
-
-/* Login Box */
-form {
-    background: white;
-    padding: 30px;
-    width: 300px;
-    border-radius: 12px;
-    box-shadow: 0 8px 20px rgba(0, 0, 0, 0.15);
-}
-
-h1 {
-    text-align: center;
-    margin-bottom: 20px;
-    color: #333;
-
+    if (role === "coach") {
+        window.location.href = "dashboard/coach.html";
+    } else if (role === "parent") {
+        window.location.href = "dashboard/parent.html";
+    } else {
+        window.location.href = "dashboard/player.html";
+    }
 }
